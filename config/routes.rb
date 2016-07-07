3840839aeaac2f6db0d7db3a2b8f881b6a192deb
Oiro <moses.oiro.okello@gmail.com>
2016-07-07 14:44:02 +0300

    Add password resets & email configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  # get 'password_resets/new'
+
+  # get 'password_resets/edit'
+
   # get 'sessions/new'
 
   # get 'users/new'
 
   # get 'static_pages/home'
 
-  root 'static_pages#home'
+  # root 'static_pages#home'
 
   # get 'static_pages/help'
 
@@ -17,11 +21,15 @@ Rails.application.routes.draw do
 
 
   # Rails.application.routes.draw do
+  # get 'password_resets/new'
+
+  # get 'password_resets/edit'
+
   # get 'sessions/new'
 
-#get 'users/new'
+  #get 'users/new'
 
-  # root 'static_pages#home'
+  root 'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
@@ -31,6 +39,7 @@ Rails.application.routes.draw do
   delete 'logout' => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
 
   # The priority is based upon order of creation: first created -> highest priority.
