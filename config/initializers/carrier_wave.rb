7e22dc8692895e56fd4d0bee744771b1448240d7
Oiro <moses.oiro.okello@gmail.com>
2016-07-22 17:39:14 +0300

    Sent Email via gmail smtp

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,16 +1,5 @@
 if Rails.env.production?
-<<<<<<< HEAD
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      # Configuration for Amazon S3
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :region                => ENV['S3_REGION']
-    }
-    config.fog_directory     =  ENV['S3_BUCKET']
-  end
-=======
+
 		CarrierWave.configure do |config|
 		config.fog_credentials = {
 # Configuration for Amazon S3
@@ -19,6 +8,6 @@ if Rails.env.production?
 			:aws_secret_access_key => ENV['14345_SAFCOM']
 }
 		config.fog_directory   = ENV['S3_BUCKET']
-	end
->>>>>>> user-microposts
+	
+end
 end
